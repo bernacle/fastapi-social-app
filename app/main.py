c42f766c975c69445aff88148e91c1951cfd6fad
Bruno Simplicio <bruno@notabene.id>
2024-03-30 10:41:27 -0400

    schemas and response models

--- app/main.py
+++ app/main.py
@@ -1,8 +1,6 @@
-import time
 from fastapi import Response, FastAPI, status, HTTPException, Depends
-from pydantic import BaseModel
-from . import models
-from .database import engine, SessionLocal, get_db
+from . import models, schemas
+from .database import engine, get_db
 from sqlalchemy.orm import Session
 
 models.Base.metadata.create_all(bind=engine)
@@ -10,35 +8,14 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = True
-
-
-my_posts = [{"title": "Post 1", "content": "This is the content of post 1", "published": True, "id": 1},
-            {"title": "Post 2", "content": "This is the content of post 2", "published": False, "id": 2},
-            {"title": "Post 3", "content": "This is the content of post 3", "published": True, "id": 3},]
-
-
-def find_post(id):
-    for post in my_posts:
-        if post["id"] == id:
-            return post
-
-def find_post_index(id):
-    for i, p in enumerate(my_posts):
-        if p["id"] == id:
-            return i
-
 @app.get("/posts")
 def get_posts(db: Session = Depends(get_db)):
 
     posts = db.query(models.Post).all()
     return {"data": posts}
 
-@app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_posts(post: Post, db: Session = Depends(get_db)):
+@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
     
 
     new_post = models.Post(**post.model_dump())
@@ -46,7 +23,7 @@ def create_posts(post: Post, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_post)
 
-    return {"data": new_post}
+    return new_post
 
 
 @app.get("/posts/{id}")
@@ -71,8 +48,9 @@ def delete_post(id: int, db: Session = Depends(get_db)):
 
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
+
 @app.put("/posts/{id}")
-def update_post(id: int, updated_post: Post, db: Session = Depends(get_db)):
+def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
     post_query = db.query(models.Post).filter(models.Post.id == id)
 
     post = post_query.first()
