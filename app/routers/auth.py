7cc7a12857e651570e4462fe36cc45302acbc65a
Bruno Simplicio <bruno@notabene.id>
2024-03-30 14:31:56 -0400

    applying jwt token

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,23 +1,22 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
-from ..database import get_db
-from ..schemas import UserLogin
-from ..models import User
-from ..utils import verify
+from .. import database, schemas, models, oauth2, utils
 
 router = APIRouter(
     tags=["Authentication"]
 )
 
 @router.post("/login")
-def login(user_credentials: UserLogin, db: Session = Depends(get_db)):
-    user = db.query(User).filter(User.email == user_credentials.email).first()
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
+    user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
 
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Credentials")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Credentials")
     
-    if not verify(user_credentials.password, user.password):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Credentials")
+    if not utils.verify(user_credentials.password, user.password):
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Credentials")
 
+    access_token = oauth2.create_access_token(data = {"user_id": user.id})
 
-    return {"token": "token"}
+    return {"access_token": access_token, "token_type": "bearer" }
--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import Optional
 from pydantic import BaseModel, EmailStr
 
 
@@ -34,3 +35,11 @@ class UserOut(BaseModel):
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    id: Optional[str] = None
